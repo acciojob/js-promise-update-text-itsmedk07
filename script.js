2ab1f6101eefadf3d8bccab753c4c2315353f281
itsmedk07 <126004945+itsmedk07@users.noreply.github.com>
2023-07-21 04:50:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+const update= document.getElementId('output')
+  const value= new promise((resolve,reject)=>{
+	  setTimeout(()=>resolve("hiiiii"),1000)
+  })
+update(value).innertext="Hello, world!"
